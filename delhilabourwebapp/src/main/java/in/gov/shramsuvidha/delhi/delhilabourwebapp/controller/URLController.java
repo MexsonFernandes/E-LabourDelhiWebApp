efada9729a85e4506fb2e364605415059317311b
Mexson Fernandes <robomex2020@gmail.com>
2018-03-23 23:40:07 +0530

    restcontroller

--- delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
@@ -2,9 +2,10 @@ package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class URLController {
 
