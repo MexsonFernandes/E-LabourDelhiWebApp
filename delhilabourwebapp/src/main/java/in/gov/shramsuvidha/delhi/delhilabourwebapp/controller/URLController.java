4d336a4e77cbd4440733ea521d23a6c8e37d3cbf
Mexson Fernandes <robomex2020@gmail.com>
2018-03-24 15:12:03 +0530

    template added

--- delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
@@ -1,16 +1,26 @@
 package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class URLController {
 
     @RequestMapping("/")
-    public String home(){
+    public String home(Map<String, Object> model){
+        model.put("message", "HowToDoInJava Reader !!");
         return "index";
+    }
+}
+
+@RestController
+class RestControl{
+
+    @RequestMapping("/test")
+    public String home(){
+        return "test";
     }
 }
