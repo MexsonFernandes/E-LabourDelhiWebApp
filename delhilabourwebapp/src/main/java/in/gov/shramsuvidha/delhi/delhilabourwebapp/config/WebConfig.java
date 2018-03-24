d42a83c074d241f93103c3306bc5b6a9c2607bcc
Mexson Fernandes <robomex2020@gmail.com>
2018-03-24 18:08:48 +0530

    jsp file added

---
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/config/WebConfig.java
@@ -0,0 +1,31 @@
+package in.gov.shramsuvidha.delhi.delhilabourwebapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("in.gov.shramsuvidha.delhi.delhilabourwebapp")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/JSP/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
