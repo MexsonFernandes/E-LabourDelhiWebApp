5b0e2e10a72cc7dab08979660ff2002df9913686
anshcena <anshcena.1996@gmail.com>
2018-03-28 14:29:42 +0530

    DASHBOARD WELCOME

--- delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/URLController.java
@@ -1,25 +1,17 @@
 package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
 
-import java.util.Map;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Map;
 
-//import in.gov.shramsuvidha.delhi.delhilabourwebapp.EmailServiceImpl;
+
 
 @Controller
 public class URLController {
 
     @RequestMapping("/")
-    public String home(Map<String, Object> model) {
+    public String home(Map<String, Object> model){
         model.put("message", "HowToDoInJava Reader !!");
-        //EmailServiceImpl send = new EmailServiceImpl();
-        try {
-            //send.sendSimpleMessage("robomex2020@gmail.com","test","test");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return "index";
     }
 
@@ -28,15 +20,6 @@ public class URLController {
         return "dashboard";
     }
 
-    @RequestMapping(value = "/ServletRegister", method = RequestMethod.GET)
-    public String registration(Model model) {
-        model.addAttribute("userForm", "some data to be sent");
-        return "redirect:error";
-    }
-    @RequestMapping(value = "/ServletLogin", method = RequestMethod.GET)
-    public String login(Model model) {
-        return "redirect:error";
-    }
     @RequestMapping("/admin")
     public String admin(){
         return "admin";
