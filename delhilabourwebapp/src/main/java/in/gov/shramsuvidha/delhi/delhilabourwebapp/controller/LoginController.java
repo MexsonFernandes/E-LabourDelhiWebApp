7f2f803e9b6d9a2d1ae6f7ba63a6ccace157c69f
Mexson Fernandes <robomex2020@gmail.com>
2018-03-28 15:15:16 +0530

    header footer index

--- delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/LoginController.java
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/LoginController.java
@@ -1,16 +1,28 @@
 package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
 
 import in.gov.shramsuvidha.delhi.delhilabourwebapp.Database.HibernateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 
-@RestController
+
+@Controller
 public class LoginController{
-
+    @Autowired
+    HttpSession hs;
     @RequestMapping(value = "/ServletLogin", method = RequestMethod.POST)
     public String Login(@RequestParam("lData") String data) {
         HibernateUtil obj=new HibernateUtil();
-        return obj.verify(data);
+        if (obj.verify(data).equals("User Exist"))
+        {
+
+
+            hs.setAttribute("username","USER!");
+            //return "redirect:dashboard";;
+            return "redirect:dashboard";
+        }else
+            return "";
     }
 }
