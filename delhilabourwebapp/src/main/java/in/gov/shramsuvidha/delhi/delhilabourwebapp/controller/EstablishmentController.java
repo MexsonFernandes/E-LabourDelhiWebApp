c3e05473dc5e129e957b581b2a092c07123977a0
ANUPAMA MISHRA <8615102@apiit.edu.in>
2018-03-30 11:12:57 +0530

    adding establishment api

---
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/EstablishmentController.java
@@ -0,0 +1,24 @@
+package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
+
+import in.gov.shramsuvidha.delhi.delhilabourwebapp.Database.HibernateUtil;
+import in.gov.shramsuvidha.delhi.delhilabourwebapp.model.Establishment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EstablishmentController {
+
+    @RequestMapping(value = "/createEstablishment", method = RequestMethod.POST)
+    public String createEstablishment(@RequestBody Establishment establishment){
+        try {
+            HibernateUtil hu=new HibernateUtil();
+            hu.saveEstablishment(establishment);
+            return "establishment created successfully";
+        }catch(Exception e) {
+            return e.toString();
+        }
+    }
+}
