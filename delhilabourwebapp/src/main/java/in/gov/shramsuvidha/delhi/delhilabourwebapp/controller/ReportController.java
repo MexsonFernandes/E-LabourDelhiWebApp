2ffb4ec39d2e94336b635605e0a8531b0997d4bd
ANUPAMA MISHRA <8615102@apiit.edu.in>
2018-03-31 07:27:42 +0530

    pics added

---
+++ delhilabourwebapp/src/main/java/in/gov/shramsuvidha/delhi/delhilabourwebapp/controller/ReportController.java
@@ -0,0 +1,7 @@
+package in.gov.shramsuvidha.delhi.delhilabourwebapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportController {
+}
